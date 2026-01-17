73144ef3ae77fdc57e9505c30cc3773e22d781d6
robinhood0107 <pjj4678@gmail.com>
2026-01-17 19:46:57 +0900

    test

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # npm-server-boot-script-custom
 npm-server-boot-script-custom shell파일
+
+## 요구사항
+- Ubuntu 20.04 LTS 이상
+
+## 사용법
+- 인스턴스 생성 후, 터미널에 다음을 입력하고 실행
+
+
+## shell 실행시 설정하는 항목들
+- apt update / upgrade
+- Timezone: Asia/Seoul
+- Virtual Memory: 4GB
+- Port Open: 21, 22, 80, 81, 443, 8000, 9443
+- Docker
+  - Docker-compose
+  - Network: npmnetwork
+  - Container
+    - Nginx Proxy Manager
+    - Portainer
+
+## 도커 내부의 네트워크
+- `npm-net` 네트워크로 **Nginx Proxy Manager**와 **Portainer**가 연결됩니다. **Nginx Proxy Manager**에서 새로운 컨테이너에 프록시 연결을 하려면 컨테이너 네트워크에 `npmnetwork`을 추가가 필요.
+- `sudo docker network connect npmnetwork 컨테이너이름`
+- 같은 네트워크에 있는 컨테이너는 **Nginx Proxy Manager**의 hostname/ip에 컨테이너 이름을 입력하여 편하게 프록시 연결할 수 있습니다.
---
+++ npm-server-boot-script-custom.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y upgrade
+
+sudo timedatectl set-timezone Asia/Seoul
+
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo echo '/swapfile swap swap defaults 0 0' | sudo tee -a /etc/fstab
+
+sudo ufw allow 21
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 8000
+sudo ufw allow 9443
+yes | sudo ufw enable
+sudo systemctl enable ufw.service
+sudo ufw restart
+sudo ufw status
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+sudo apt-get -y install docker-compose
+
+sudo mkdir -p /data/npm
+sudo docker run -d -p 80:80 -p 443:443 -p 81:81 --name npm --restart=unless-stopped -v /data:/data -v /letsencrypt:/etc/letsencrypt jc21/nginx-proxy-manager:latest
+
+sudo mkdir -p /data/portainer
+sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+sudo docker network create npmnetwork
+sudo docker network connect npmnetwork npm
+sudo docker network connect npmnetwork portainer
+
+ip="$(curl --silent icanhazip.com)"
+echo ""
+echo ""
+echo "부트 스크립트 실행 완료"
+echo ""
+echo "- Nginx Proxy Manager"
+echo "http://$ip:81"
+echo ""
+echo "- NPM 기본 로그인은 다음과 같음"
+echo "Email: admin@example.com"
+echo "Password: changemeplz"
+echo ""
+echo "- Portainer"
+echo "http://$ip:8000"
+echo ""
+echo "NPM과 다른 컨테이너를 연결 시 npmnetwork 네트워크에 연결 필요함"
